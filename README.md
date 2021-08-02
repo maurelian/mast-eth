bd66af68f67b7f95eb0c9a748cce925e1c0d339b
Maurelian <maurelian@protonmail.ch>
2021-08-02 14:05:55 -0400

    contracts: Add some basic contract structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# MAST on Ethereum
+
+A simple experimental project to implement some of the key functionality of the [Merkleized Abstract Script Tree](https://river.com/learn/terms/m/merkelized-alternative-script-tree-mast/), which is part of Bitcoin's Taproot upgrade.
+
+Specifically:
+
+> Merkle branches let us only reveal the actually executed part of the script to the blockchain, as opposed to all possible ways a script can be executed. Among the various known mechanisms for implementing this, one where the Merkle tree becomes part of the script's structure directly maximizes the space savings, so that approach is chosen.
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,16 @@
-import { task } from "hardhat/config";
+import { task } from 'hardhat/config'
 
-import "@nomiclabs/hardhat-waffle";
+import '@nomiclabs/hardhat-waffle'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -19,5 +19,5 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.4',
+}
